8e588de6187eb1ff1f54a31502aa55fdc82976df
Richard Vincent <rich@richardvincent.com>
2015-12-22 23:57:09 +0000

    Capi Edit

--- models.py
+++ models.py
@@ -3,8 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime
-from sqlalchemy import Column, Integer, String, DateTime, Float, ForeignKey,\
-    Date
+from sqlalchemy import Column, Integer, String, DateTime, Float, ForeignKey
 """Bike industry Management System Module
 
 Please use the classes and functions contained in this module to design your
@@ -19,54 +18,56 @@ session = Session()
 Base = declarative_base()
 
 
-class Bike_Shop(Base):
-    __tablename__ = "Bike_Shop"
+class Shop(Base):
+    __tablename__ = "shops"
 
-    store_id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
+    margin = Column(Float, nullable=False)
 
 
-class Bicycle(Base):
-    __tablename__ = "Bicycle"
+class Bike(Base):
+    __tablename__ = "bikes"
 
-    model_name = Column(String, primary_key=True)
-    model_description = Column(String, nullable=False)
-    cost_to_produce = Column(Float, nullable=False)
+    id = Column(String, primary_key=True)
+    description = Column(String, nullable=False)
+    cost = Column(Float, nullable=False)
     weight = Column(Float, nullable=False)
 
 
-class Customers(Base):
+class Customer(Base):
     __tablename__ = "customers"
 
-    customer_number = Column(Float, primary_key=True)
-    customer_name = Column(String, nullable=False)
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
     budget = Column(Float, nullable=False)
 
 
-class Inventoy(Base):
+class Inventory(Base):
     __tablename__ = "inventory"
-    inv_id = Column(Integer, primary_key=True)
-    store_id = Column(Integer, ForeignKey('Bike_Shop.store_id'), nullable=False)
-    model_name = Column(String, ForeignKey('Bicycle.model_name'),
+    id = Column(Integer, primary_key=True)
+    store_id = Column(Integer, ForeignKey('bikes.id'), nullable=False)
+    store = relationship("Shop", backref="inventory")
+    model_name = Column(String, ForeignKey('bikes.id'),
                         nullable=False)
+    model = relationship("Bike", backref="inventory")
     last_update = Column(DateTime, default=datetime.utcnow)
 
 
 class Transaction(Base):
     __tablename__ = "transactions"
 
-    transaction_id = Column(Integer, primary_key=True)
-    customer_number = Column(Integer, ForeignKey('customers.customer_number'),
+    id = Column(Integer, primary_key=True)
+    customer_number = Column(Integer, ForeignKey('customers.id'),
                              nullable=False)
-    model_name = Column(String, ForeignKey('Bicycle.model_name'),
+    customer = relationship("Customer", backref="transactions")
+    model_name = Column(String, ForeignKey('bikes.id'),
                         nullable=False)
+    model = relationship("Bike", backref="transactions")
     date_time = Column(DateTime, default=datetime.utcnow)
     price = Column(Float, nullable=False)
-    store_id = Column(Integer, ForeignKey('Bike_Shop.store_id'), nullable=False)
+    store_id = Column(Integer, ForeignKey('shops.id'), nullable=False)
+    store = relationship("Shop", backref="transactions")
 
 
 Base.metadata.create_all(engine)
-
-
-
-
